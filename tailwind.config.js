5af234642c1a841b91dbcafbd8c8dee0575ec85e
Eric Junior <eric.faria2003@gmail.com>
2025-01-03 10:07:18 -0300

    Up ambinete

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./src/**/*.{html,js}"],
+    theme: {
+      extend: {
+        colors: {
+          primary: "#FFD700", // Amarelo
+          background: "#121212", // Preto
+        },
+      },
+    },
+    plugins: [],
+  };
+  
