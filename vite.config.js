dc3c7f149a2dfdf6894e6065dadbddc1df7665e6
Eric Junior <eric.faria2003@gmail.com>
2025-01-03 10:18:43 -0300

    Init index

---
+++ vite.config.js
@@ -0,0 +1,8 @@
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  root: './src', // Define a pasta raiz como 'src'
+  build: {
+    outDir: '../dist', // Define a pasta de saída para 'dist'
+  },
+});
