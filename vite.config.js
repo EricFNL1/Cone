aceb4bc33514af5db04ae6e160b12a31793de696
Eric Junior <eric.faria2003@gmail.com>
2025-01-04 20:00:43 -0300

    Teste

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,12 @@ export default defineConfig({
   root: './src', // Define a pasta raiz como 'src'
   build: {
     outDir: '../dist', // Define a pasta de saída para 'dist'
+    rollupOptions: {
+      output: {
+        entryFileNames: 'assets/[name].js', // Nome dos arquivos JS
+        chunkFileNames: 'assets/[name].js', // Nome dos arquivos de chunks
+        assetFileNames: 'assets/styles.css', // Define o nome fixo para o CSS gerado
+      },
+    },
   },
 });
