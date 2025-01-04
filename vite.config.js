3269c504cf6c5fdbe4b606e754dbd097ffc822eb
Eric Junior <eric.faria2003@gmail.com>
2025-01-04 20:06:48 -0300

    Teste

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
       output: {
         entryFileNames: 'assets/[name].js', // Nome dos arquivos JS
         chunkFileNames: 'assets/[name].js', // Nome dos arquivos de chunks
-        assetFileNames: 'assets/styles.css', // Define o nome fixo para o CSS gerado
+        assetFileNames: 'styles.css', // Coloca o CSS diretamente na pasta dist
       },
     },
   },
